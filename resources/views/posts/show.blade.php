129c4e78c40fbd413d1e90a19fc58368518183cd
Jamie Howard <jamiehowardco@gmail.com>
2019-07-24 22:14:44 -0400

    Moved blog post content to be larger on mobile.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,12 +6,13 @@
 	
 	<div class="mb-4">
 		<div class="heading w-full text-center lg:text-left">{{$post->title}}</div>
-		<p class="text-gray-600 text-xs relative -mt-2">
+
+		<p class="text-gray-600 text-xs relative lg:-mt-2">
 			Published on {{$post->created_at->format('d F Y ')}}
 		</p>
 	</div>
 
-	<p class="leading-relaxed">{{$post->content}}</p>
 </div>
 
+<p class="leading-relaxed text-left">{{$post->content}}</p>
 @endsection
