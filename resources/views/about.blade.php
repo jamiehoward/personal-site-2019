a69c95603fdc66500f4059a0661173f877a0993b
Jamie Howard <jamiehowardco@gmail.com>
2019-07-30 10:28:09 -0400

    Corrected bug with CTA button padding

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
 			Hey, my name is Jamie. By day, I'm the Director of Development at <a href="http://thinkorange.com" target="_blank">Orange</a>, where I get to write code that helps kids have a better future. Outside of work, <a href="https://instagram.com/jamiehowarddraws" target="_blank">I draw a lot</a>, am starting an <a href="https://morrowmade.com" target="_blank">organization that enables innovators to do great work</a>, and like to <a href="/blog">journal</a> about my explorations in learning to take Jesus seriously.
 		</p>
 
-		<a class="bg-gray-200 text-gray-600  hover:bg-gray-600 hover:text-white py-4 px-10" href="https://twitter.com/messages/compose?recipient_id=14348689&text=Hey+there!" target="_blank">
+		<a class="text-xs md:text-md bg-gray-200 text-gray-600 hover:bg-gray-600 hover:text-white p-4 md:px-10" href="https://twitter.com/messages/compose?recipient_id=14348689&text=Hey+there!" target="_blank">
 			<span class="font-bold pr-1">Want to chat?</span> 
 			Hit me up on Twitter!
 		</a>
